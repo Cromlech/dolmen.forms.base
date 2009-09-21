37d7afb07590a931a3038306762664f0e690bc2e
Souheil Chelfouh <trollfot@draugen.(none)>
2009-09-21 14:54:02 +0200

    Changed the models to echoes the changes in Grok 1.0b2

--- src/dolmen/forms/base/__init__.py
+++ src/dolmen/forms/base/__init__.py
@@ -7,9 +7,8 @@ from z3c.form.interfaces import DISPLAY_MODE, INPUT_MODE
 
 # megrok.z3cform imports
 from megrok.z3cform.base import Field, Fields
-from megrok.z3cform.base import widget, button, action
+from megrok.z3cform.base import cancellable, widget, button, action
 from megrok.z3cform.base import apply_data_event, applyChanges, extends
 from megrok.z3cform.base import Form, AddForm, EditForm, DisplayForm
 from megrok.z3cform.base import PageForm, PageAddForm, PageEditForm
 from megrok.z3cform.base import PageDisplayForm, WidgetTemplate
-from megrok.z3cform.layout import cancellable
