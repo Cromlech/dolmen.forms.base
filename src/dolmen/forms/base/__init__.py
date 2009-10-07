fd42b41043d4a268a88694cf7e2ceb83048c285e
Souheil CHELFOUH <trollfot@vittra.org>
2009-10-07 21:17:49 +0200

    Corrected imports

--- src/dolmen/forms/base/__init__.py
+++ src/dolmen/forms/base/__init__.py
@@ -1,15 +1,13 @@
-## -*- coding: utf-8 -*-
-
 # direct z3cform imports
-from z3c.form.form import handleActionError
+from z3c.form import button, action, validator
+from z3c.form.form import extends, handleActionError
+from z3c.form.field import Field, Fields, FieldWidgets
 from z3c.form.interfaces import DISPLAY_MODE, INPUT_MODE
 from z3c.form.interfaces import IFormLayer, IFieldWidget
 from z3c.form.interfaces import IForm, IAddForm, IEditForm, IDisplayForm
 
 # megrok.z3cform imports
-from megrok.z3cform.base import Field, Fields
-from megrok.z3cform.base import cancellable, widget, button, action
-from megrok.z3cform.base import apply_data_event, applyChanges, extends
-from megrok.z3cform.base import Form, AddForm, EditForm, DisplayForm
-from megrok.z3cform.base import PageForm, PageAddForm, PageEditForm
-from megrok.z3cform.base import PageDisplayForm, WidgetTemplate
+from megrok.z3cform.base.utils import *
+from megrok.z3cform.base.components import *
+from megrok.z3cform.base.directives import *
+from megrok.z3cform.base.interfaces import *
