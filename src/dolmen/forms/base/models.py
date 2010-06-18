5bef50b7181725547c6708c964bff27f719eb73b
Souheil CHELFOUH <trollfot@gmail.com>
2010-06-19 01:32:56 +0200

    Update the ApplicationForm model, to allow right language i18n, to compute form invariants and to display it as a group of errors.

--- src/dolmen/forms/base/models.py
+++ src/dolmen/forms/base/models.py
@@ -3,8 +3,58 @@
 
 from grokcore.component import baseclass
 from megrok.layout.components import UtilityView
+from zeam.form.base.errors import Errors, Error
+from zeam.form.base.interfaces import ICollection
+from zeam.form.base.markers import NOT_EXTRACTED
+from zeam.form.base.widgets import getWidgetExtractor
 from zeam.form.layout import Form
+from zeam.form.ztk.validation import InvariantsValidation
+from zope.i18n.interfaces import IUserPreferredLanguages
 
 
 class ApplicationForm(Form, UtilityView):
     baseclass()
+
+    @property
+    def i18nLanguage(self):
+        prefs = IUserPreferredLanguages(self.request, None)
+        if prefs is not None:
+            return prefs.getPreferredLanguages()[0]
+        return None
+
+    @property
+    def formError(self):
+        error = self.errors.get(self.prefix, None)
+        if error is None or ICollection.providedBy(error):
+            return error
+        return [error]
+
+    def extractData(self, fields=None):
+        if fields is None:
+            fields = self.fields
+
+        if self._FormData__extracted is not NOT_EXTRACTED:
+            return (self._FormData__extracted, self.errors)
+        self._FormData__extracted = data = dict()
+
+        for field in fields:
+            # Global extraction and data dict creation
+            extractor = getWidgetExtractor(field, self, self.request)
+            value, error = extractor.extract()
+            if error is None:
+                error = field.validate(value, self.context)
+
+            if error is not None:
+                self.errors.append(Error(error, field.identifier))
+            data[field.identifier] =  value
+
+        # Invariants validation
+        invalids = InvariantsValidation(fields).validate(data)
+        if len(invalids):
+            self.errors.append(Errors(
+                *[Error(invalid.message) for invalid in invalids],
+                identifier=self.prefix))
+
+        if len(self.errors):
+            return (data, self.errors)
+        return (data, None)
