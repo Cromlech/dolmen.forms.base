08de2174e3e0b2266b39000ea70a692f3f157cd0
Christian Klinger <cklinger@novareto.de>
2012-10-16 16:26:52 +0200

    forgot tests

--- src/dolmen/forms/base/tests.py
+++ src/dolmen/forms/base/tests.py
@@ -2,8 +2,18 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+from zeam.form import ztk
 from zope.testing import doctest
+from zope.component import eventtesting
 from zeam.form.ztk.testing import FunctionalLayer
+from zope.app.wsgi.testlayer import BrowserLayer
+
+
+class MyFunctionalLayer(BrowserLayer):
+
+    def setUp(self):
+        super(MyFunctionalLayer, self).setUp()
+        eventtesting.setUp()
 
 
 def test_suite():
@@ -12,7 +22,7 @@ def test_suite():
     readme = doctest.DocFileSuite(
         'README.txt',
         optionflags=(doctest.ELLIPSIS + doctest.NORMALIZE_WHITESPACE))
-    readme.layer = FunctionalLayer
+    readme.layer = MyFunctionalLayer(ztk)
     suite = unittest.TestSuite()
     suite.addTest(readme)
     return suite
