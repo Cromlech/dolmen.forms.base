1101ab9a8564f9b83e72de20c14b3fd7aa6216e8
Souheil CHELFOUH <trollfot@gmail.com>
2012-11-15 17:09:47 +0100

    Readying release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from os.path import join
 from setuptools import setup, find_packages
 
 name = 'dolmen.forms.base'
-version = '2.4.1'
+version = '2.4.2'
 readme = open('README.txt').read()
 history = open(join('docs', 'HISTORY.txt')).read()
 
